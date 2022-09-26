feb7fe55ca37cfd8b9b77611111599344d9240a8
nana <986593516@qq.com>
2022-09-26 14:56:53 +0800

    test

--- main/main.go
+++ main/main.go
@@ -6,11 +6,28 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-func main() {
-	database, err := sqlx.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/mysql")
+var database *sqlx.DB
+var userName = "root"
+var userPasswd = "123456"
+var sqlAddr = "127.0.0.1:3306"
+
+//init
+/*
+ * @author 	nana
+ * @description init函数sql连接
+ */
+func init() {
+	var err error
+	dataSourceName := userName + ":" + userPasswd + "@tcp(" + sqlAddr + ")/mysql"
+
+	database, err = sqlx.Open("mysql", dataSourceName)
 	if err != nil {
 		fmt.Println("open mysql failed,", err)
 		return
 	}
+	fmt.Println("open mysql succeed,")
+}
+
+func main() {
 	fmt.Println(database)
 }
